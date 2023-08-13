fd1fad043e36bb7cdc5cf2dcdcbf57483d7e17f8
Allan Browning <allan@inov8design.net>
2023-08-13 22:28:45 +1000

    Added image upload

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const PORT = process.env.PORT;
 const express = require('express');  // Import express.js
 const app = express();  // The app object conventionally denotes the Express application. Create it by
                         // calling the top-level express() function exported by the Express module.
@@ -5,7 +7,8 @@ const app = express();  // The app object conventionally denotes the Express app
 const path = require('path');
 
 const http = require('http').Server(app);
-
+var fs = require('fs');
+const formidable = require('formidable');
 var cors = require('cors')
 app.use(cors());
 app.use (express.json());   // Mounts the specified middleware function at the
@@ -13,14 +16,17 @@ app.use (express.json());   // Mounts the specified middleware function at the
                             // requested path matches path. In this case we are using middleware to parse
                             // JSON data
 
-
+app.use('/images',express.static('userimages'));
 // app.use(express.static(path.join(__dirname, '../dist/week5tut/'))); // Serve
 // static content for the app from the “public”
 
 // Target the build version of the angular app
 // created in the "dist" directory:
 
+//Route for uploading images.
+require('./routes/api-uploads.js')(app,formidable,fs,path);
+
 // Route for checking user credentials
 require('./routes/api-login.js')(app,path);
 // Start the server listening on port 3000. Output message to console once server has started.(diagnostic only)
-require('./listen.js')(http);
+require('./listen.js')(http,PORT);
